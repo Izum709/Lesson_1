1ccbbc99b3d2980d27d0abb2ed539400fc52ca65
Alexandr Vinogradov <izum709@.com>
2023-04-09 15:06:05 +0300

    Add greeting

---
+++ Greeting/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write ("Привет, представся ");
+string username = Console.ReadLine();
+if(username.ToLower() == "alex")
+{
+    Console.WriteLine("Ура это же Alex");
+
+}
+else 
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}
+
+
