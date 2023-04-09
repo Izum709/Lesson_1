9e6ee8ef193de3cc2d5ec5be13a4c498e71c0d81
Alexandr Vinogradov <izum709@.com>
2023-04-09 14:36:14 +0300

    Add sum random

---
+++ Random/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Привет математик)  ");
+int numberA = new Random().Next(1, 10);
+Console.Write(numberA);
+Console.Write("+");
+int numberB = new Random().Next(1,10);
+Console.Write(numberB);
+int sum = numberA + numberB;
+Console.Write(" = ");
+Console.Write(sum);
+Console.Write("!");
